8ed4696c830de0f9d49d759b31bf3340ee412640
umeharaharuki <jasper1116lopez@icloud.com>
2022-05-23 16:38:13 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  with_options presence: true do
+    validates :nickname
+    validates :profile
+    validates :country_id
+  end
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
